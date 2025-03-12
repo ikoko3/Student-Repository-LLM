9f0ca4f99c24f58ffd4475e375a672dc726e85ae
Ioannis (Giannis) Siriopoulos <57215843+ivsyriopoulos@users.noreply.github.com>
2025-03-12 12:43:28 +0100

    telika o giannhs eftaige

--- student_service/constants.js
+++ student_service/constants.js
@@ -11,13 +11,13 @@ const DYNAMO_DB_INDEXES = {
   STUDENT_DATE_INDEX: "Date-index",
 };
 
-module.exports = {
-  TABLES,
-  DYNAMO_DB_INDEXES,
-  GPT_OPTIONS,
-};
-
 const GPT_OPTIONS = {
   model: "gpt-3.5-turbo",
   max_tokens: 400,
+};
+
+module.exports = {
+  TABLES,
+  DYNAMO_DB_INDEXES,
+  GPT_OPTIONS
 };
